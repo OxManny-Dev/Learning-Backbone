99721c904a8221ddbba33295827e218cba16c571
Emmanuel <emmanueljucaban@gmail.com>
2016-09-24 12:24:59 -0700

    Learned how to make ajax calls to server

--- js/backbone-ajax-request.js
+++ js/backbone-ajax-request.js
@@ -1,9 +1,4 @@
 
-// In the first few sections, we do all the coding here.
-// Later, you'll see how to organize your code into separate
-// files and modules.
-
-
 // Models > Connecting to Server
 
 
