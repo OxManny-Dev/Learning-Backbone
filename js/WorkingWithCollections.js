0cebcf1e1dc841971fbb5547bc5a4a27b3550956
Emmanuel <emmanueljucaban@gmail.com>
2016-09-24 14:01:33 -0700

    Learned how to work with collections

--- js/WorkingWithCollections.js
+++ js/WorkingWithCollections.js
@@ -1,7 +1,3 @@
-// In the first few sections, we do all the coding here.
-// Later, you'll see how to organize your code into separate
-// files and modules.
-
 // Working with collections
 
 var Song = Backbone.Model.extend();
