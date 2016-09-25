c164f6dbfddc058e0a9305b5d68b35229046abcb
Emmanuel <emmanueljucaban@gmail.com>
2016-09-24 20:23:59 -0700

    Learned how to use templating with underscore

--- js/Backbone-templating.js
+++ js/Backbone-templating.js
@@ -1,7 +1,3 @@
-// In the first few sections, we do all the coding here.
-// Later, you'll see how to organize your code into separate
-// files and modules.
-
 // Templating using Underscore
 
 var Song = Backbone.Model.extend();
