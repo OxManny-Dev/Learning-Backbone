f287748805913cdeec4cf2dcd52f8fcc911dee3f
Emmanuel <emmanueljucaban@gmail.com>
2016-09-24 18:12:09 -0700

    Learned how to pass models and collections into views

--- js/Views-Passing-Model-Collection.js
+++ js/Views-Passing-Model-Collection.js
@@ -1,7 +1,3 @@
-// In the first few sections, we do all the coding here.
-// Later, you'll see how to organize your code into separate
-// files and modules.
-
 // Passing Models and Collections to views
 
 var Song = Backbone.Model.extend();
